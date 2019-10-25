4ddf500a07522d6c72224e9e72e49ad26a457103
Nikolay Kelov <n_kelov@yahoo.com>
2019-10-25 10:21:59 +0200

    Problem 02

---
+++ 01.DB_Apps_Introduction/01.ADO.NET_Exercise/P02.VillianNames/P02StartUp.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace P02.VillianNames
+{
+    class P02StartUp
+    {
+        private static string connectionString = 
+            "Server=DESKTOP-7JEJ5UL\\SQLEXPRESS01;"+
+            "Database=MinionsDB;"+
+            "Integrated Security=true;";
+
+        private static SqlConnection conection = new SqlConnection(connectionString);
+        static void Main(string[] args)
+        {
+            conection.Open();
+
+            using (conection)
+            {
+                string queryText = @"  SELECT v.Name, COUNT(mv.VillainId) AS MinionsCount  
+                                       FROM Villains AS v 
+                                       JOIN MinionsVillains AS mv ON v.Id = mv.VillainId 
+                                       GROUP BY v.Id, v.Name 
+                                       HAVING COUNT(mv.VillainId) > 3 
+                                       ORDER BY COUNT(mv.VillainId)";
+
+                SqlCommand command = new SqlCommand(queryText, conection);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["Name"]} - {reader["MinionsCount"]}");
+                    }
+                }
+            }
+        }
+    }
+}
