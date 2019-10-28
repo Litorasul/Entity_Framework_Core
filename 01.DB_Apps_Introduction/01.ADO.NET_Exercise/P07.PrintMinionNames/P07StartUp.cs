71dee6d861f9cc94ecf1b97a94391b11763c10b5
Nikolay Kelov <n_kelov@yahoo.com>
2019-10-28 12:44:28 +0100

    Problem 07

---
+++ 01.DB_Apps_Introduction/01.ADO.NET_Exercise/P07.PrintMinionNames/P07StartUp.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace P07.PrintMinionNames
+{
+    class P07StartUp
+    {
+        private static string connectionString =
+            "Server=DESKTOP-7JEJ5UL\\SQLEXPRESS01;" +
+            "Database=MinionsDB;" +
+            "Integrated Security=true;";
+
+        private static SqlConnection connection = new SqlConnection(connectionString);
+        static void Main(string[] args)
+        {
+            connection.Open();
+
+            using (connection)
+            {
+                string queryText = @"SELECT Name FROM Minions";
+
+                SqlCommand command = new SqlCommand(queryText, connection);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                List<string> namesList = new List<string>();
+
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        namesList.Add((string)reader["Name"]);
+                    }
+                }
+
+                while (namesList.Count > 0)
+                {
+                    Console.WriteLine(namesList[0]);
+                    namesList.RemoveAt(0);
+
+                    if (namesList.Count > 0)
+                    {
+                        Console.WriteLine(namesList[namesList.Count - 1]);
+                        namesList.RemoveAt(namesList.Count - 1);
+                    }
+                }
+            }
+        }
+    }
+}
