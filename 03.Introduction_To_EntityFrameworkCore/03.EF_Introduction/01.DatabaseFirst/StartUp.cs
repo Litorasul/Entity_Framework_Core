5461c6a142ec7dc3cc97cae693d0a23746d62b0c
Nikolay Kelov <n_kelov@yahoo.com>
2019-11-04 13:25:00 +0100

    Problem 03 and Problem 04

--- 03.Introduction_To_EntityFrameworkCore/03.EF_Introduction/01.DatabaseFirst/StartUp.cs
+++ 03.Introduction_To_EntityFrameworkCore/03.EF_Introduction/01.DatabaseFirst/StartUp.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Text;
+using SoftUni.Data;
+using System.Linq;
+
 
 namespace SoftUni
 {
@@ -6,7 +10,58 @@ namespace SoftUni
     {
         public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            SoftUniContext context = new SoftUniContext();
+
+            string result = GetEmployeesWithSalaryOver50000(context);
+
+            Console.WriteLine(result);
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context
+                .Employees
+                .OrderBy(e => e.FirstName)
+                .Where(e => e.Salary > 50000)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.Salary
+                });
+
+            foreach (var e in employees)
+            {
+                sb
+                    .AppendLine($"{e.FirstName} - {e.Salary:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context
+                .Employees
+                .OrderBy(e => e.EmployeeId)
+                .Select(e => new
+                {
+                    Name = String.Join(" ", e.FirstName, e.LastName, e.MiddleName),
+                    e.JobTitle,
+                    e.Salary
+                });
+
+            foreach (var e in employees)
+            {
+                sb
+                    .AppendLine($"{e.Name} {e.JobTitle} {e.Salary:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+
         }
     }
 }
