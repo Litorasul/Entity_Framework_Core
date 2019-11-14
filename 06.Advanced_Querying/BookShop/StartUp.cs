f0ed5f611ee713a669d69a162d3f679a3bfac55c
Nikolay Kelov <n_kelov@yahoo.com>
2019-11-14 15:29:39 +0100

    Problem 02

--- 06.Advanced_Querying/BookShop/StartUp.cs
+++ 06.Advanced_Querying/BookShop/StartUp.cs
@@ -1,5 +1,7 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Text;
+using BookShop.Models.Enums;
 
 namespace BookShop
 {
@@ -13,6 +15,8 @@ namespace BookShop
             using (var db = new BookShopContext())
             {
                //DbInitializer.ResetDatabase(db);
+
+               Console.WriteLine(GetGoldenBooks(db));
             }
         }
 
@@ -28,6 +32,30 @@ namespace BookShop
                     b.Title
                 })
                 .OrderBy(b => b.Title)
+                .ToList();
+
+            foreach (var book in books)
+            {
+                sb.AppendLine(book.Title);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var books = context
+                .Books
+                .Where(b => b.EditionType == EditionType.Gold)
+                .Where(b => b.Copies < 5000)
+                .Select(b => new
+                {
+                    b.BookId,
+                    b.Title
+                })
+                .OrderBy(b => b.BookId)
                 .ToList();
 
             foreach (var book in books)
