95e0e660cc7637b96f924ef5487366d53f3ad0ff
Nikolay Kelov <n_kelov@yahoo.com>
2019-11-23 11:33:05 +0100

    Problem 09

---
+++ 09.XML_Processing/CarDealer - Skeleton/CarDealer/StartUp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using AutoMapper;
+using CarDealer.Data;
+using CarDealer.Dtos.Import;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Mapper.Initialize(cfg => cfg.AddProfile<CarDealerProfile>());
+
+            using (var db = new CarDealerContext())
+            {
+                //db.Database.EnsureDeleted();
+                //db.Database.EnsureCreated();
+
+                var inputXml = File.ReadAllText("./../../../Datasets/suppliers.xml");
+
+                var result = ImportSuppliers(db, inputXml);
+
+                Console.WriteLine(result);
+            }
+        }
+
+        //Problem 09 - 100%
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(ImportSuppliersDto[]),
+                new XmlRootAttribute("Suppliers"));
+
+            ImportSuppliersDto[] suppliersDtos;
+
+            using (var reader = new StringReader(inputXml))
+            {
+                suppliersDtos = (ImportSuppliersDto[]) xmlSerializer.Deserialize(reader);
+            }
+
+            var suppliers = Mapper.Map<Supplier[]>(suppliersDtos);
+
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Length}";
+        }
+
+    }
+}
