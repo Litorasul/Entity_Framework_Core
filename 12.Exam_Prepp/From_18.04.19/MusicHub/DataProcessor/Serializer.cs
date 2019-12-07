eb0938abac37704c627823da2de04718ee55b3e7
Nikolay Kelov <n_kelov@yahoo.com>
2019-12-07 11:45:00 +0200

    Problem 03

--- 12.Exam_Prepp/From_18.04.19/MusicHub/DataProcessor/Serializer.cs
+++ 12.Exam_Prepp/From_18.04.19/MusicHub/DataProcessor/Serializer.cs
@@ -1,4 +1,14 @@
-﻿namespace MusicHub.DataProcessor
+﻿using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using MusicHub.DataProcessor.ExportDtos;
+using Newtonsoft.Json;
+using Formatting = Newtonsoft.Json.Formatting;
+
+namespace MusicHub.DataProcessor
 {
     using System;
 
@@ -8,13 +18,60 @@
     {
         public static string ExportAlbumsInfo(MusicHubDbContext context, int producerId)
         {
-            
-            throw new NotImplementedException();
+            var albums = context.Albums
+                .Where(a => a.ProducerId == producerId)
+                .OrderByDescending(a => a.Price)
+                .Select(a => new
+                {
+                    AlbumName = a.Name,
+                    ReleaseDate = a.ReleaseDate.ToString("MM/dd/yyyy"),
+                    ProducerName = a.Producer.Name,
+                    Songs = a.Songs
+                        .OrderByDescending(s => s.Name)
+                        .ThenBy(s => s.Writer)
+                        .Select(s => new
+                    {
+                        SongName = s.Name,
+                        Price = s.Price.ToString("F2"),
+                        Writer = s.Writer.Name
+                    }),
+                    AlbumPrice = a.Songs.Sum(p => p.Price).ToString("F2")
+                })
+                .ToList();
+
+            return JsonConvert.SerializeObject(albums, Formatting.Indented);
         }
 
         public static string ExportSongsAboveDuration(MusicHubDbContext context, int duration)
         {
-            throw new NotImplementedException();
+            var songs = context
+                .Songs
+                .Where(s => s.Duration.TotalSeconds >= duration)
+                .Select(s => new ExportSongDto
+                {
+                    SongName = s.Name,
+                    Writer = s.Writer.Name,
+                    Performer = s.SongPerformers
+                        .Select(p => $"{p.Performer.FirstName} {p.Performer.LastName}")
+                        .FirstOrDefault(),
+                    AlbumProducer = s.Album.Producer.Name,
+                    Duration = s.Duration.ToString("c", CultureInfo.InvariantCulture)
+
+                })
+                .OrderBy(s => s.SongName)
+                .ThenBy(s => s.Writer)
+                .ThenBy(s => s.Performer)
+                .ToArray();
+
+            var xmlSerializer = new XmlSerializer(typeof(ExportSongDto[]),
+                new XmlRootAttribute("Songs"));
+
+            var sb = new StringBuilder();
+            var namespaces = new XmlSerializerNamespaces(new[] { XmlQualifiedName.Empty });
+
+            xmlSerializer.Serialize(new StringWriter(sb), songs, namespaces);
+
+            return sb.ToString().TrimEnd();
         }
     }
 }
